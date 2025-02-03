9648bedd40d0e9ec035f0ce77e8d4d0e509a7468
dzarenafixers <dzarenafixersinfo@gmail.com>
2025-02-03 21:30:21 +0100

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using PluginFramework; // ✅ تأكد أن هذا هو `Namespace` الصحيح
+
+namespace AdminTools
+{
+    public class AdminToolsPlugin : IPluginWithCommands
+    {
+        public string Name => "Admin Tools";
+        public string Version => "1.0.0";
+
+        public void OnLoad()
+        {
+            ServerConsole.AddLog($"✅ [{Name}] Plugin loaded successfully!", ConsoleColor.Green);
+        }
+
+        public List<string> GetCommands()
+        {
+            return new List<string>
+            {
+                "adminkick <player>",
+                "adminban <player> <duration>",
+                "adminmute <player>",
+                "adminunmute <player>"
+            };
+        }
+    }
+}
